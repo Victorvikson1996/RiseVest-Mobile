6f0c22a1c409684ce83cd795703488a2ca2d6477
Victor Ezea <victorvikson@gmail.com>
2023-04-13 14:07:19 +0100

    Set up

---
+++ src/screens/index.js
@@ -0,0 +1,7 @@
+import FeedScreen from "./FeedScreen";
+import MoreScreen from "./MoreScreen";
+import OnBoardingScreen from "./OnBoardingScreen";
+import PlansScreen from "./PlansScreen";
+import WalletScreen from "./WalletScreen";
+
+export { FeedScreen, MoreScreen, OnBoardingScreen, PlansScreen, WalletScreen };
